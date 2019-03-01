d931583474beab1030629083eec70cdccb5c3658
yanrui <1131411057@qq.com>
2019-03-01 15:15:11 +0800

    project init

---
+++ src/main/java/com/yangchenle/electricityconsumptionsystem/ElectricityConsumptionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yangchenle.electricityconsumptionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityConsumptionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityConsumptionSystemApplication.class, args);
+	}
+
+}
