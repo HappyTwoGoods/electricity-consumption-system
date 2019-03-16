cd6b4a3b0a7fa4e1a58f590a0a0673d4d8bcd4b4
Liang <1144351205@qq.com>
2019-03-16 16:23:15 +0800

    liang

---
+++ src/main/java/com/yangchenle/electricityconsumptionsystem/controller/DeductionRecordController.java
@@ -0,0 +1,8 @@
+package com.yangchenle.electricityconsumptionsystem.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeductionRecordController {
+
+}
